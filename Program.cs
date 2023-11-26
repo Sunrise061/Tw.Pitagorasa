7c4fae90c3d4c07f5a346b56a847fec4bfb063ab
ST26-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 17:39:49 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            var a = Math.Pow(3.5 ,2);
+            var b = Math.Pow(6 , 2);
+            
+            var c = Math.Sqrt(a + b);
+            Console.WriteLine("długość przeciwprostokątnej wynosi:");
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
